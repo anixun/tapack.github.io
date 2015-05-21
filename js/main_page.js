17acf13f11dfa218aad10ccedb86661b31db84d5
hunke <hunke@ukr.net>
2015-05-21 12:50:25 +0300

    Modified home page, Added close modal dialog

--- js/main_page.js
+++ js/main_page.js
@@ -2,5 +2,8 @@ $(document).ready(function(){
     $("#openForm").click(function(){
         $("#formSubmit").show(500);
     });
+    $("#closeForm").click(function(){
+        $("#formSubmit").hide(500);
+    })
 
 });
