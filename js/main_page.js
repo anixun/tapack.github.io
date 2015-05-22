1f52e7ced41b69cd4db4fcb1e83ea9c5b5725635
hunke <hunke@ukr.net>
2015-05-22 18:12:51 +0300

    dent effect added

--- js/main_page.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#openForm").click(function(){
-        $("#formSubmit").show(500);
-    });
-    $("#closeForm").click(function(){
-        $("#formSubmit").hide(500);
-    })
-
-});
